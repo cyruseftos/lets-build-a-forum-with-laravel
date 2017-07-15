7d697819455077bfcac5843ba9fe22ad28cb24f2
Cyrus Eftos <cyruseftos@gmail.com>
2017-07-15 15:27:04 +0800

    Split ThreadsTests into seperate tests. 'a_user_can_view_all_threads' & 'a_user_can_read_a_single_thread'

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -12,12 +12,17 @@ class ThreadsTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
-    public function a_user_can_browse_threads()
+    public function a_user_can_view_all_threads()
     {
         $thread = factory('App\Thread')->create();
 
         $response = $this->get('/threads');
         $response->assertSee($thread->title);
+    }
+
+    public function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
 
         $response = $this->get('/threads/' . $thread->id);
         $response->assertSee($thread->title);
